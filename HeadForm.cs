98599a1ad3a170f449462169ccb2d97b6148258e
Alexey Mukin <amukin@cheby.ru>
2014-10-02 16:42:52 +0400

    Проект + БД с адресами

---
+++ HeadForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSZN
+{
+    public partial class HeadForm : Form
+    {
+        public HeadForm()
+        {
+            InitializeComponent();
+        }
+
+        private void выгрузитьДанныеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
